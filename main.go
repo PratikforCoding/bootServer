22f8368b24b31e79bb8cc7313221c6e80a5a752d
PratikforCoding <kotalpratik@gmail.com>
2023-09-06 17:22:43 +0530

    added hit middleware
    Signed-off-by: PratikforCoding <kotalpratik@gmail.com>

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
+
 
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -24,15 +30,35 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func assetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	response := `<pre>
+	<a href="logo.png">logo.png</a>
+	</pre>`
+
+	_, err := w.Write([]byte(response))
+	if err != nil {
+		http.Error(w, "Failed to write resoponse!", http.StatusInternalServerError)
+		return
+	}
+}
+func (apiCfg *apiConfig)metricsHandler(w http.ResponseWriter, r *http.Request) {
+	
+	hits := apiCfg.fileserverHits
+	w.Header().Set("Content-Type", "text/plain: charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", hits)
+	
+}
 func main() {
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
-
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/app/assets/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./assets/logo.png")
-	})
+	apiCfg := &apiConfig{}
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/app/assets", assetHandler)
 	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
 
 	server := &http.Server{
 		Addr: ":8080",
